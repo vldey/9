5a8825b42506311857b868ff4f24f636476f34bb
vldey <agungalghazali046@gmail.com>
2026-03-09 15:19:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://s.shopee.co.id/9UwZpo5ine", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029Vb7CxEGKmCPQpUpl072I");
+}
+
+function openFacebookPage() {
+    window.open("https://www.facebook.com/profile.php?id=61583248358924");
+}
+const video = document.getElementById("video");
+const overlay = document.getElementById("videoOverlay");
+
+let overlayClicked = false; 
+
+// Overlay muncul di detik tertentu
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 1 && !overlayClicked) {
+        overlay.classList.add("show");
+    }
+});
+
+// Klik overlay
+overlay.addEventListener("click", () => {
+    overlayClicked = true;              
+    overlay.style.display = "none";   
+    overlay.classList.remove("show");
+
+    // Aksi setelah klik
+    window.open("https://s.shopee.co.id/9UwZpo5ine", "_blank");
+});
+
+
+
+
+
+
+
+
+
+
+
+
